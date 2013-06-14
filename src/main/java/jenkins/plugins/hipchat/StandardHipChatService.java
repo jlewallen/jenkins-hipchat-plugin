3dc28a6f825d95fa238e3d67c95f8660956d1220
steve miller <steven.miller@intergraph.com>
2013-06-14 12:33:17 -0400

    changed to use static Jenkins class and renamed method

--- src/main/java/jenkins/plugins/hipchat/StandardHipChatService.java
+++ src/main/java/jenkins/plugins/hipchat/StandardHipChatService.java
@@ -6,6 +6,9 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import jenkins.model.Jenkins;
+import hudson.ProxyConfiguration;
+
 public class StandardHipChatService implements HipChatService {
 
     private static final Logger logger = Logger.getLogger(StandardHipChatService.class.getName());
@@ -29,7 +32,7 @@ public class StandardHipChatService implements HipChatService {
     public void publish(String message, String color) {
         for (String roomId : roomIds) {
             logger.info("Posting: " + from + " to " + roomId + ": " + message + " " + color);
-            HttpClient client = getJenkinsClient();
+            HttpClient client = getHttpClient();
             String url = "https://" + host + "/v1/rooms/message?auth_token=" + token;
             PostMethod post = new PostMethod(url);
 
@@ -49,14 +52,15 @@ public class StandardHipChatService implements HipChatService {
         }
     }
     
-    private HttpClient getJenkinsClient() {
+    private HttpClient getHttpClient() {
         HttpClient client = new HttpClient();
-        if (Hudson.getInstance() != null) {
-            ProxyConfiguration proxy = Hudson.getInstance().proxy;
+        if (Jenkins.getInstance() != null) {
+            ProxyConfiguration proxy = Jenkins.getInstance().proxy;
             if (proxy != null) {
                 client.getHostConfiguration().setProxy(proxy.name, proxy.port);
             }
-		}
+        }
+        return client;
     }
 
     private String shouldNotify(String color) {
