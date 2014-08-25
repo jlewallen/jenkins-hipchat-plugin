fbd6f34f3dc23de69377eb772bafb272a2d3aa15
Darrin Holst <darrinholst@gmail.com>
2014-08-24 19:52:59 -0500

    Add defaults for host and from user
    Closes #8

--- src/main/java/jenkins/plugins/hipchat/StandardHipChatService.java
+++ src/main/java/jenkins/plugins/hipchat/StandardHipChatService.java
@@ -1,30 +1,32 @@
 package jenkins.plugins.hipchat;
 
+import hudson.ProxyConfiguration;
+import jenkins.model.Jenkins;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.PostMethod;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import jenkins.model.Jenkins;
-import hudson.ProxyConfiguration;
-
 public class StandardHipChatService implements HipChatService {
-
     private static final Logger logger = Logger.getLogger(StandardHipChatService.class.getName());
+    private static final String DEFAULT_HOST = "api.hipchat.com";
+    private static final String[] DEFAULT_ROOMS = new String[0];
+    private static final String DEFAULT_FROM = "Build Server";
 
     private String host;
     private String token;
     private String[] roomIds;
     private String from;
 
-    public StandardHipChatService(String host, String token, String roomId, String from) {
+    public StandardHipChatService(String host, String token, String roomIds, String from) {
         super();
-        this.host = host;
+        this.host = host == null ? DEFAULT_HOST : host;
         this.token = token;
-        this.roomIds = roomId.split(",");
-        this.from = from;
+        this.roomIds = roomIds == null ? DEFAULT_ROOMS : roomIds.split(",");
+        this.from = from == null ? DEFAULT_FROM : from;
     }
 
     public void publish(String message) {
@@ -47,7 +49,7 @@ public class StandardHipChatService implements HipChatService {
                 post.getParams().setContentCharset("UTF-8");
                 int responseCode = client.executeMethod(post);
                 String response = post.getResponseBodyAsString();
-                if(responseCode != HttpStatus.SC_OK || ! response.contains("\"sent\"")) {
+                if (responseCode != HttpStatus.SC_OK || !response.contains("\"sent\"")) {
                     logger.log(Level.WARNING, "HipChat post may have failed. Response: " + response);
                 }
             } catch (Exception e) {
@@ -60,12 +62,15 @@ public class StandardHipChatService implements HipChatService {
 
     private HttpClient getHttpClient() {
         HttpClient client = new HttpClient();
+
         if (Jenkins.getInstance() != null) {
             ProxyConfiguration proxy = Jenkins.getInstance().proxy;
+
             if (proxy != null) {
                 client.getHostConfiguration().setProxy(proxy.name, proxy.port);
             }
         }
+
         return client;
     }
 
@@ -73,7 +78,15 @@ public class StandardHipChatService implements HipChatService {
         return color.equalsIgnoreCase("green") ? "0" : "1";
     }
 
-    void setHost(String host) {
-        this.host = host;
+    public String getHost() {
+        return host;
+    }
+
+    public String[] getRoomIds() {
+        return roomIds;
+    }
+
+    public String getFrom() {
+        return from;
     }
 }
--- src/test/java/jenkins/plugins/hipchat/StandardHipChatServiceTest.java
+++ src/test/java/jenkins/plugins/hipchat/StandardHipChatServiceTest.java
@@ -1,18 +1,50 @@
 package jenkins.plugins.hipchat;
 
-import jenkins.plugins.hipchat.StandardHipChatService;
-import org.junit.Before;
 import org.junit.Test;
 
-public class StandardHipChatServiceTest {
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
-    /**
-     * Publish should generally not rethrow exceptions, or it will cause a build job to fail at end.
-     */
+public class StandardHipChatServiceTest {
     @Test
     public void publishWithBadHostShouldNotRethrowExceptions() {
         StandardHipChatService service = new StandardHipChatService("badhost", "token", "room", "from");
-        service.setHost("hostvaluethatwillcausepublishtofail");
         service.publish("message");
+    }
+
+    @Test
+    public void shouldSetADefaultHost() {
+        StandardHipChatService service = new StandardHipChatService(null, "token", "room", "from");
+        assertEquals("api.hipchat.com", service.getHost());
+    }
+
+    @Test
+    public void shouldBeAbleToOverrideHost() {
+        StandardHipChatService service = new StandardHipChatService("some.other.host", "token", "room", "from");
+        assertEquals("some.other.host", service.getHost());
+    }
+
+    @Test
+    public void shouldSplitTheRoomIds() {
+        StandardHipChatService service = new StandardHipChatService(null, "token", "room1,room2", "from");
+        assertArrayEquals(new String[]{"room1", "room2"}, service.getRoomIds());
+    }
+
+    @Test
+    public void shouldNotSplitTheRoomsIfNullIsPassed() {
+        StandardHipChatService service = new StandardHipChatService(null, "token", null, "from");
+        assertArrayEquals(new String[0], service.getRoomIds());
+    }
+
+    @Test
+    public void shouldProvideADefaultFrom() {
+        StandardHipChatService service = new StandardHipChatService(null, "token", "room", null);
+        assertEquals("Build Server", service.getFrom());
+    }
+
+    @Test
+    public void shouldBeAbleToOverrideFrom() {
+        StandardHipChatService service = new StandardHipChatService(null, "token", "room", "from");
+        assertEquals("from", service.getFrom());
     }
 }
